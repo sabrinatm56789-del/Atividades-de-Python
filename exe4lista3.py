f708864dcacd22df81c2e2a57e8cf3fdba0873de
sabrinatm56789-del <sabrinatm56789@gmail.com>
2026-04-13 18:43:17 -0300

    Add files via upload

---
+++ exe4lista3.py
@@ -0,0 +1,23 @@
+C= int(input(' Insira o código:'))
+Q= int (input( 'Quantidade:'))
+if C== 100:
+    VP= 1.20 * Q
+    print (f'Valor a ser pago: R$ {VP:.2f}')
+elif C== 101:
+    VP= 1.30 * Q
+    print (f'Valor a ser pago: R$ {VP:.2f}')
+elif C== 102:
+    VP= 1.50 * Q
+    print (f'Valor a ser pago: R$ {VP:.2f}')
+elif C== 103:
+    VP= 1.20 * Q
+    print (f'Valor a ser pago: R$ {VP:.2f}')
+elif C== 104:
+    VP= 1.70 * Q
+    print (f'Valor a ser pago: R$ {VP:.2f}')
+elif C== 105:
+    VP= 2.20 * Q
+    print (f'Valor a ser pago: R$ {VP:.2f}')
+elif C== 106:
+    VP= 1.00 * Q
+    print (f'Valor a ser pago: R$ {VP:.2f}')
